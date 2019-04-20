54213abdcdba5e66ba1afcf674cfba273a5fceb4
Mark Gardner <mark.gardner@endurance.com>
2019-04-20 17:26:06 -0500

    Updates

---
+++ precache-manifest.1fed7243929629915b86ab5606e84c0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2259ffe137d5e9eff00",
+    "url": "/clicky-game/static/css/main.a0ce0a5e.chunk.css"
+  },
+  {
+    "revision": "f2259ffe137d5e9eff00",
+    "url": "/clicky-game/static/js/main.e40b9508.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "0ae60febaa11a19db6b3c1de8c9059bb",
+    "url": "/clicky-game/index.html"
+  }
+];
